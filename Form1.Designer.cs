263d6f1f4d3075444df21eacc80d6d3181e8f36a
Asus <Asus@LAPTOP-AV0QSD1C>
2023-08-20 23:31:55 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace music_player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.playlist1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.labelElapsedTime = new System.Windows.Forms.Label();
+            this.labelTotalTime = new System.Windows.Forms.Label();
+            this.LabelArtistSong = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.Location = new System.Drawing.Point(27, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "browse";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(485, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(647, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "next";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(323, 352);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "previous";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(404, 352);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "stop";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(566, 352);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "pause";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // playlist1
+            // 
+            this.playlist1.BackColor = System.Drawing.Color.Snow;
+            this.playlist1.ForeColor = System.Drawing.Color.Black;
+            this.playlist1.FormattingEnabled = true;
+            this.playlist1.ItemHeight = 16;
+            this.playlist1.Location = new System.Drawing.Point(12, 67);
+            this.playlist1.Name = "playlist1";
+            this.playlist1.Size = new System.Drawing.Size(227, 356);
+            this.playlist1.TabIndex = 6;
+            this.playlist1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(143, 98);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(24, 21);
+            this.checkedListBox1.TabIndex = 7;
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(314, 281);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(408, 56);
+            this.trackBar.TabIndex = 9;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // labelElapsedTime
+            // 
+            this.labelElapsedTime.AutoSize = true;
+            this.labelElapsedTime.Location = new System.Drawing.Point(270, 281);
+            this.labelElapsedTime.Name = "labelElapsedTime";
+            this.labelElapsedTime.Size = new System.Drawing.Size(38, 16);
+            this.labelElapsedTime.TabIndex = 10;
+            this.labelElapsedTime.Text = "00:00";
+            this.labelElapsedTime.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelTotalTime
+            // 
+            this.labelTotalTime.AutoSize = true;
+            this.labelTotalTime.Location = new System.Drawing.Point(728, 281);
+            this.labelTotalTime.Name = "labelTotalTime";
+            this.labelTotalTime.Size = new System.Drawing.Size(38, 16);
+            this.labelTotalTime.TabIndex = 11;
+            this.labelTotalTime.Text = "00:00";
+            this.labelTotalTime.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // LabelArtistSong
+            // 
+            this.LabelArtistSong.AutoSize = true;
+            this.LabelArtistSong.Location = new System.Drawing.Point(401, 237);
+            this.LabelArtistSong.Name = "LabelArtistSong";
+            this.LabelArtistSong.Size = new System.Drawing.Size(252, 16);
+            this.LabelArtistSong.TabIndex = 12;
+            this.LabelArtistSong.Text = "THIS IS WHATS CURRETNLY PLAYING";
+            this.LabelArtistSong.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LabelArtistSong);
+            this.Controls.Add(this.labelTotalTime);
+            this.Controls.Add(this.labelElapsedTime);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.playlist1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "lalala this plays music";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox playlist1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Label labelElapsedTime;
+        private System.Windows.Forms.Label labelTotalTime;
+        private System.Windows.Forms.Label LabelArtistSong;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,520 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using NAudio.Wave;
+using TagLib;
+
+namespace music_player
+{
+    public enum PlaybackState
+    {
+        Stopped,
+        Playing,
+        Paused
+    }
+
+    public partial class Form1 : Form //class for the UI
+    {
+        private struct SongInfo  //to keep track of the current song 
+        {
+            public string Title;
+            public string Artist;
+            //public string Lenght;
+        }
+
+        private SongInfo currentSong = new SongInfo();
+        private Playlist playlist;
+        private MusicPlayer musicPlayer;
+        private string currentFilePath = null;
+        private Timer updateTimer;
+
+        private void RefreshPlaylist()
+        {
+            playlist1.Items.Clear();
+            foreach (var song in playlist.Songs)
+            {
+                playlist1.Items.Add(song);
+            }
+        }
+
+        private void InitializeTimer()
+        {
+            if (updateTimer == null)
+            {
+                updateTimer = new Timer();
+                updateTimer.Interval = 1000;
+                updateTimer.Tick += updateTimer_Tick;
+            }
+
+            updateTimer.Start();
+        }
+
+        private void StopTimer()
+        {
+            updateTimer?.Stop();
+        }
+       
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen; //ensures that the form starts in the middle
+            CenterContent();
+            playlist = new Playlist();
+            musicPlayer = new MusicPlayer(playlist);
+
+            musicPlayer.TrackBarUpdateEvent += HandleTrackBarUpdate;
+            musicPlayer.SongStoppedEvent += MusicPlayer_SongStopped;
+            musicPlayer.SongStartedEvent += MusicPlayer_SongStarted;
+    }
+
+        private void CenterContent()
+        {
+            //something to center the content
+        }
+
+        //event handlers and ui stuff
+
+        private void button1_Click(object sender, EventArgs e) //button for browsing
+        {
+            //file browsing logic, allowing the user to select audio
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "MP3 Files|*.mp3|WAV Files|*.wav|All Files|*.*";
+                openFileDialog.Title = "Select an audio file";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) // Show the OpenFileDialog and check if the user selected a file
+                {
+                    string selectedFilePath = openFileDialog.FileName;
+
+                    Song newSong = new Song
+                    {
+                        FilePath = selectedFilePath,
+                        Title = Path.GetFileNameWithoutExtension(selectedFilePath) //setting a title 
+                    };
+
+                    playlist.AddSong(newSong);
+                    RefreshPlaylist();
+                    //musicPlayer.Play(newSong.FilePath);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //play btn
+        {
+            switch (musicPlayer.CurrentState)
+            {
+                case PlaybackState.Paused:
+                    musicPlayer.Play(); // Resume playback
+                    break;
+
+                case PlaybackState.Stopped:
+                    if (currentFilePath != null)
+                    {
+                        musicPlayer.Play(currentFilePath); // Play the last selected song
+                    }
+                    else
+                    {
+                        var firstSong = playlist.Songs.FirstOrDefault();
+                        if (firstSong != null)
+                        {
+                            currentFilePath = firstSong.FilePath;
+                            musicPlayer.Play(firstSong.FilePath); // If no song was previously selected, play the first song in the playlist
+                        }
+                    }
+                    break;
+
+                case PlaybackState.Playing:
+                    // You can decide what action to perform if a song is already playing when the play button is pressed.
+                    break;
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e) //next btn
+        {
+            Song nextSong = playlist.GetNextSong();
+            if (nextSong != null)
+            {
+                currentFilePath = nextSong.FilePath;
+                musicPlayer.Play(nextSong.FilePath);
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) //previous btn
+        {
+            Song prevSong = playlist.GetPreviousSong();
+            if (prevSong != null)
+            {
+                currentFilePath = prevSong.FilePath;
+                musicPlayer.Play(prevSong.FilePath);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) //stop btn
+        {
+            musicPlayer.Stop();
+        }
+
+        private void button6_Click(object sender, EventArgs e) //pause btn
+        {
+            musicPlayer.Pause();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = playlist1.SelectedIndex;
+            if (selectedIndex >= 0)
+            {
+                playlist.CurrentSongIndex = selectedIndex;
+                Song selectedSong = playlist.GetCurrentSong();
+                musicPlayer.Play(selectedSong.FilePath);
+
+            }
+        }
+
+        private void UpdateButtonStates()
+        {
+            button2.Enabled = currentFilePath != null;
+            button3.Enabled = playlist.CurrentSongIndex < playlist.Songs.Count - 1;
+            button4.Enabled = playlist.CurrentSongIndex > 0; // Enable previous if there are songs before
+            button5.Enabled = musicPlayer.CurrentState == PlaybackState.Playing; // Enable stop if playing
+            button6.Enabled = musicPlayer.CurrentState == PlaybackState.Playing; // Enable pause if playing
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            if (musicPlayer.CurrentState == PlaybackState.Playing || musicPlayer.CurrentState == PlaybackState.Paused)
+            {
+                int newSeconds = trackBar.Value;
+                musicPlayer.Seek(newSeconds);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void updateTimer_Tick(object sender, EventArgs e)
+        {
+            if (musicPlayer.CurrentState == PlaybackState.Playing)
+            {
+                TimeSpan currentTime = musicPlayer.CurrentTime;
+                TimeSpan totalTime = musicPlayer.TotalTime;
+
+                labelElapsedTime.Text = currentTime.ToString(@"mm\:ss");
+                labelTotalTime.Text = totalTime.ToString(@"mm\:ss");
+                trackBar.Value = (int)currentTime.TotalSeconds; // totalTime.TotalSeconds) * trackBar.Maximum);
+
+            }
+        }
+
+        private void HandleTrackBarUpdate(object sender, TrackBarUpdateEventArgs e)
+        {
+            trackBar.Maximum = e.Maximum;
+            trackBar.Value = e.Value;
+        }
+
+        private void MusicPlayer_SongStarted(object sender, EventArgs e)
+        {
+            InitializeTimer();
+
+            Song currentSong = playlist.GetCurrentSong();
+            if (currentSong != null)
+            {
+                LabelArtistSong.Text = $"{currentSong.Artist} - {currentSong.Title}";
+            }
+        }
+
+        private void MusicPlayer_SongStopped(object sender, EventArgs e)
+        {
+            StopTimer();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private Song GetSongMetadata (string filepath)
+        {
+            Song song = new Song();
+            try
+            {
+                var file = TagLib.File.Create(filepath);
+                song.Title = file.Tag.Title ?? "title";
+                song.Artist = file.Tag.FirstPerformer ?? "artist";
+                song.Genre = file.Tag.FirstGenre ?? "genre";
+                song.FilePath = filepath;
+
+                if (file.Tag.Pictures.Length > 0)
+                {
+                    var artwork = file.Tag.Pictures[0].Data;
+                    song.Artwork = artwork.Data;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error playing the file: {ex.Message}", "Playback Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            return song;
+
+        }
+    }
+
+
+    //class representing songs
+    public class Song
+    {
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string FilePath { get; set; }
+        public byte[] Artwork { get; set; }
+                                          
+        public override string ToString()
+        {
+            return $"{Artist} - {Title}";
+        }
+    }
+
+    public class Playlist
+    {
+        public event EventHandler SongStoppedEvent;
+
+        public List<Song> Songs { get; } = new List<Song>();
+        public int CurrentSongIndex = -1; // deafault to -1 if no song is selected
+
+        public Song GetCurrentSong()
+        {
+            if (CurrentSongIndex >= 0 && CurrentSongIndex < Songs.Count)
+            {
+                return Songs[CurrentSongIndex];
+            }
+            return null;
+        }
+
+        public Song GetNextSong()
+        {
+            if (CurrentSongIndex + 1 < Songs.Count)
+            {
+                CurrentSongIndex++;
+                return GetCurrentSong();
+            }
+            return null;
+        }
+
+        public Song GetPreviousSong()
+        {
+            if (CurrentSongIndex - 1 >= 0)
+            {
+                CurrentSongIndex--;
+                return GetCurrentSong();
+            }
+            return null;
+        }
+
+        public void AddSong(Song song)
+        {
+            Songs.Add(song);
+            CurrentSongIndex = Songs.Count - 1;
+        }
+    }
+
+    public class Artist 
+    {
+        public string Name { get; set; }
+        private List<Song> songs = new List<Song>();
+        public List<Song> Songs { get { return songs; } }
+
+        public void AddSong(Song newSong) 
+        {
+            songs.Add(newSong);
+        }
+    }
+
+        public class TrackBarUpdateEventArgs : EventArgs
+        {
+        public int Maximum { get; set; }
+        public int Value { get; set; }
+        }
+
+        public delegate void TrackBarUpdateHandler(object sender, TrackBarUpdateEventArgs e);
+           
+    public class MusicPlayer
+    {
+        public event TrackBarUpdateHandler TrackBarUpdateEvent;
+        public event EventHandler SongStartedEvent;
+        public event EventHandler RequestStopTimer;
+        public event EventHandler SongStoppedEvent;
+
+        private IWavePlayer waveOutDevice; // Represents the device used for audio playback.
+        private AudioFileReader audioFileReader; //Used to read the audio file specified by its path.
+        private Playlist playlist;
+
+        public MusicPlayer(Playlist playlist)
+        {
+            this.playlist = playlist;
+            waveOutDevice = new WaveOut();
+            waveOutDevice.PlaybackStopped += WaveOutDevice_PlaybackStopped;
+        }
+
+        private void WaveOutDevice_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            if (CurrentState == PlaybackState.Playing)
+            {
+                Song nextSong = playlist.GetNextSong();
+                if (nextSong != null)
+                {
+                    Play(nextSong.FilePath);
+                }
+            }
+        }
+        public PlaybackState CurrentState { get; private set; } = PlaybackState.Stopped;  //the logic of music playing goes here
+
+        public void Play()  //method to play from playlist
+        {
+            if (CurrentState == PlaybackState.Paused)
+            {
+                waveOutDevice.Play();
+                CurrentState = PlaybackState.Playing;
+            }
+            else if (CurrentState == PlaybackState.Stopped)
+            {
+                Song currentSong = playlist.GetCurrentSong();
+
+                if (currentSong != null)
+                {
+                    Play(currentSong.FilePath);
+                }
+            }
+        }
+
+        public void Play(string filePath)  //method to play specific file
+        {
+            try
+            {
+                Stop();
+
+                waveOutDevice = new WaveOut();
+                audioFileReader = new AudioFileReader(filePath);
+                waveOutDevice.PlaybackStopped += OnPlaybackStopped;
+                waveOutDevice.Init(audioFileReader);
+                waveOutDevice.Play();
+                CurrentState = PlaybackState.Playing;
+
+                SongStartedEvent?.Invoke(this, EventArgs.Empty);
+                TrackBarUpdateEvent?.Invoke(this, new TrackBarUpdateEventArgs 
+                {
+                    Maximum = (int)audioFileReader.TotalTime.TotalSeconds, 
+                    Value = 0 
+                }); //this raises the event
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error playing the file: {ex.Message}", "Playback Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                audioFileReader.Dispose();
+                waveOutDevice?.Dispose();
+                audioFileReader = null;
+                waveOutDevice = null;
+            }
+        }
+
+        public void Stop()
+        {
+            waveOutDevice?.Stop();
+            if (waveOutDevice != null)
+            {
+                waveOutDevice.PlaybackStopped -= OnPlaybackStopped;
+                waveOutDevice.PlaybackStopped -= WaveOutDevice_PlaybackStopped;
+            }
+
+            audioFileReader?.Dispose();
+            waveOutDevice?.Dispose();
+            audioFileReader = null;
+            waveOutDevice = null;
+
+            CurrentState = PlaybackState.Stopped;
+            SongStoppedEvent?.Invoke(this, EventArgs.Empty);
+            RequestStopTimer?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Pause()
+        {
+            if (CurrentState == PlaybackState.Playing)
+            {
+                waveOutDevice?.Pause();
+                CurrentState = PlaybackState.Paused;
+            }
+        }
+
+        public void Skip()
+        {
+
+        }
+
+        public void GoBack()
+        {
+
+        }
+        public void Seek (int seconds)
+        {
+            if (audioFileReader != null)
+            {
+                audioFileReader.CurrentTime = TimeSpan.FromSeconds(seconds); 
+            }
+        }
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            var nextSong = playlist.GetNextSong();
+            if (nextSong != null)
+            {
+                Play(nextSong.FilePath);
+            } else
+            {
+                playlist.CurrentSongIndex = 0;
+                var firstSong = playlist.GetCurrentSong();
+                if (firstSong != null)
+                {
+                    Play(firstSong.FilePath);
+                }
+            }
+        }
+
+        public TimeSpan TotalTime
+        {
+            get
+            {
+                return audioFileReader?.TotalTime ?? TimeSpan.Zero;
+            }
+        }
+
+        public TimeSpan CurrentTime
+        {
+            get
+            {
+                return audioFileReader?.CurrentTime ?? TimeSpan.Zero;
+            }
+        }
+    }
+
+    public class MusicLibrary
+    {
+        //database or smth similar goes here 
+    }
+}
